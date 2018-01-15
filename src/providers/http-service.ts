80db7e3db0b0d5fad32fbb9c40fb5da513800696
CARLITOS\Carlos Sánchez <carlosjsanchezc@gmail.com>
2018-01-15 03:45:41 -0400

    130120182

---
+++ src/providers/http-service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable()
+export class HttpService 
+{
+  api_token:string;
+  url="https://www.dc2.com.ve/opencart/upload";
+
+  categorias:any[]=[];
+  
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.categorias=[
+      { title: 'Productos' },
+      { title: 'Vestidos' }
+    ];
+
+
+  }
+ getBanner() {
+
+    return this.http.get(this.url+"/products.php?option=1");
+  }
+  getProducts(urlapi) {
+    
+    
+    return this.http.get(urlapi);
+ 
+  }  
+  getCategorias(urlapi) {
+    
+    this.categorias=[
+      { title: 'Productos' },
+      { title: 'Vestidos' }
+    ];
+    return this.http.get(urlapi);
+    
+  }
+  loginapi() 
+  {
+    
+    return this.http.get(this.url+'/apiopencart.php');
+  }
+
+   
+
+}
